ee36aa09388e87e3d9606b9fe3fe8ccd1d540b71
Jolo Gutierrez <mariano.jose.gutierrez.iii@dlsl.edu.ph>
2024-11-12 22:12:16 +0800

    adjusted input based on forcemode

--- Assets/_Project/Bounce Scene/_Scripts/RaycastController.cs
+++ Assets/_Project/Bounce Scene/_Scripts/RaycastController.cs
@@ -6,8 +6,11 @@ public class RaycastController : MonoBehaviour
 {
     private Camera _camera;
     [SerializeField] LayerMask _groundLayer, _throwLayer;
+    [SerializeField] ForceMode _forceMode = ForceMode.Impulse;
     [SerializeField] float _hitRadius = 1f;
     [SerializeField] float _hitForce = 5f;
+    RaycastHit hitInfo;
+    Ray ray;
 
     private void Awake()
     {
@@ -16,16 +19,44 @@ public class RaycastController : MonoBehaviour
 
     private void Update()
     {
-        if (!Input.GetMouseButtonDown(0)) { return; }
-        var ray = _camera.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, _groundLayer))
+        if (_forceMode == ForceMode.Impulse || _forceMode == ForceMode.VelocityChange)
         {
-            var colliders = Physics.OverlapSphere(hitInfo.point, _hitRadius, _throwLayer);
-            foreach (var collider in colliders)
+            if (Input.GetMouseButtonDown(0))
             {
-                collider.attachedRigidbody.AddForce((collider.transform.position - hitInfo.point) * _hitForce, ForceMode.Impulse);
+                ray = _camera.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity, _groundLayer))
+                {
+                    var colliders = Physics.OverlapSphere(hitInfo.point, _hitRadius, _throwLayer);
+                    foreach (var collider in colliders)
+                    {
+                        collider.attachedRigidbody.AddForce((collider.transform.position - hitInfo.point) * _hitForce, _forceMode);
+                    }
+                }
             }
         }
+        else if (_forceMode == ForceMode.Acceleration || _forceMode == ForceMode.Force)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                ray = _camera.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity, _groundLayer))
+                {
+                    Debug.DrawLine(ray.origin, hitInfo.point, Color.cyan);
+                    var colliders = Physics.OverlapSphere(hitInfo.point, _hitRadius, _throwLayer);
+                    foreach (var collider in colliders)
+                    {
+                        Debug.DrawLine(collider.transform.position, hitInfo.point, Color.red);
+                        collider.attachedRigidbody.AddForce((collider.transform.position - hitInfo.point) * _hitForce, _forceMode);
+                    }
+                }
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+
+        Gizmos.DrawWireSphere(hitInfo.point, _hitRadius);
     }
 
 }
